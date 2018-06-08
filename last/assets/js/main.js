8d4641bfbe03f5e823b218e4621b4fab6de9589c
aliciasimons <37204250+aliciasimons@users.noreply.github.com>
2018-06-08 13:55:19 +1000

    Add files via upload

---
+++ last/assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+
+  $('.e-hearts').mouseenter(function() {
+      $('.p-hearts').addClass('p-show');
+    });
+
+
+    $('.e-hearts').mouseleave(function() {
+    $('.p-hearts').removeClass('p-show');
+  });
+
+// Add jQuery here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
---
+++ last/assets/js/main4.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+setTimeout(function() {
+  $(".p-sub").addClass("show");
+}, 3000);
+
+setTimeout(function() {
+  $(".p-bye").addClass("show");
+}, 5000);
+
+setTimeout(function() {
+  $(".p-one").addClass("show");
+}, 8000);
+
+setTimeout(function() {
+  $(".p-two").addClass("show");
+}, 9000);
+
+setTimeout(function() {
+  $(".p-three").addClass("show");
+}, 2000);
+
+setTimeout(function() {
+  $(".p-four").addClass("show");
+}, 1000);
+
+setTimeout(function() {
+  $(".p-five").addClass("show");
+}, 5000);
+
+setTimeout(function() {
+  $(".p-six").addClass("show");
+}, 7000);
+
+setTimeout(function() {
+  $(".p-seven").addClass("show");
+}, 10000);
+
+setTimeout(function() {
+  $(".p-eight").addClass("show");
+}, 11000);
+
+setTimeout(function() {
+  $(".p-nine").addClass("show");
+}, 5000);
+
+setTimeout(function() {
+  $(".p-ten").addClass("show");
+}, 9000);
+
+
+setTimeout(function() {
+  $(".p-eleven").addClass("show");
+}, 10000);
+
+setTimeout(function() {
+  $(".p-twelve").addClass("show");
+}, 20000);
+
+setTimeout(function() {
+  $(".p-thirteen").addClass("show");
+}, 13000);
+
+setTimeout(function() {
+  $(".p-fourteen").addClass("show");
+}, 14000);
+
+setTimeout(function() {
+  $(".p-fifteen").addClass("show");
+}, 15000);
+
+setTimeout(function() {
+  $(".p-sixteen").addClass("show");
+}, 16000);
+
+setTimeout(function() {
+  $(".p-seventeen").addClass("show");
+}, 18000);
+
+
+setTimeout(function() {
+  $(".p-eighteen").addClass("show");
+}, 17000);
+
+setTimeout(function() {
+  $(".p-nineteen").addClass("show");
+}, 25000);
+
+setTimeout(function() {
+  $(".p-twenty").addClass("show");
+}, 210000);
+
+
+  });
